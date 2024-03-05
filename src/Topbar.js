2b8bd9bf5f310e7eb0c91c98c625d1bf947b3f27
Mau <mauriciomartinezvazquez@rocketmail.com>
2024-03-05 16:41:30 -0600

    forgot to remove that

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,6 +1,6 @@
-function Topbar({ innerRef }) {
+function Topbar() {
     return (
-        <nav id="Topbar" ref={innerRef}>
+        <nav id="Topbar">
           <h1>Where in the world?</h1>
           <label id='switch-theme-label' htmlFor='switch-theme'><span className="material-symbols-outlined">contrast</span>Change theme</label>
         </nav>
